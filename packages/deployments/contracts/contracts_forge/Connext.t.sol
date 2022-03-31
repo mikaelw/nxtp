bcb96967a8c1a7416a8dad9c61e1932cb2fd10e4
LayneHaber <layne.haber@gmail.com>
2022-03-30 21:56:32 -0600

    Create connext helper contract

--- packages/deployments/contracts/contracts_forge/Connext.t.sol
+++ packages/deployments/contracts/contracts_forge/Connext.t.sol
@@ -1,4 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.11;
+
 import "./ForgeHelper.sol";
 
 import "../contracts/Connext.sol";
@@ -11,37 +13,17 @@ import "../contracts/Connext.sol";
 // other forge commands: yarn workspace @connext/nxtp-contracts forge <CMD>
 // see docs here: https://onbjerg.github.io/foundry-book/index.html
 
-contract ConnextTest is ForgeHelper {
+contract ConnextTest is ForgeConnextHelper {
+
   // ============ Libraries ============
   using stdStorage for StdStorage;
 
   // ============ Storage ============
 
-  Connext connext;
-
-  uint256 domain = 1;
-  address bridgeRouter = address(1);
-  address tokenRegistry = address(2);
-  address wrapper = address(3);
-
   // ============ Test set up ============
 
   function setUp() public {
-    connext = new Connext();
-    connext.initialize(domain, payable(bridgeRouter), tokenRegistry, wrapper);
-  }
-
-  // ============ Utils ============
-  // https://github.com/brockelmore/forge-std
-  // specifically here with overriding mappings: https://github.com/brockelmore/forge-std/blob/99107e3e39f27339d224575756d4548c08639bc0/src/test/StdStorage.t.sol#L189-L192
-  function setApprovedRouter(address _router, bool _approved) internal {
-    uint256 writeVal = _approved ? 1 : 0;
-    stdstore.target(address(connext)).sig(connext.approvedRouters.selector).with_key(_router).checked_write(writeVal);
-  }
-
-  function setApprovedAsset(address _asset, bool _approved) internal {
-    uint256 writeVal = _approved ? 1 : 0;
-    stdstore.target(address(connext)).sig(connext.approvedAssets.selector).with_key(_asset).checked_write(writeVal);
+    setConnext();
   }
 
   // ============ addRouter ============
--- packages/deployments/contracts/contracts_forge/ForgeHelper.sol
+++ packages/deployments/contracts/contracts_forge/ForgeHelper.sol
@@ -5,12 +5,52 @@ import "../lib/forge-std/src/stdlib.sol";
 import "../lib/forge-std/src/Vm.sol";
 import "../lib/forge-std/src/console.sol";
 
+import "../contracts/Connext.sol";
+
 abstract contract ForgeHelper is DSTest {
+  // ============ Libraries ============
   using stdStorage for StdStorage;
 
+  // ============ Storage ============
   Vm public constant vm = Vm(HEVM_ADDRESS);
 
   StdStorage public stdstore;
 
   address public constant NATIVE_ASSET = address(0);
+}
+
+
+abstract contract ForgeConnextHelper is ForgeHelper {
+  // ============ Libraries ============
+  using stdStorage for StdStorage;
+
+  // ============ Storage ============
+
+  Connext connext;
+
+  uint256 domain = 1;
+  address bridgeRouter = address(11111);
+  address tokenRegistry = address(22222);
+  address wrapper = address(33333);
+  address admin = address(44444);
+
+  // ============ Utils ============
+
+  function setConnext() public {
+    connext = new Connext();
+    vm.prank(admin);
+    connext.initialize(domain, payable(bridgeRouter), tokenRegistry, wrapper);
+  }
+
+  // https://github.com/brockelmore/forge-std
+  // specifically here with overriding mappings: https://github.com/brockelmore/forge-std/blob/99107e3e39f27339d224575756d4548c08639bc0/src/test/StdStorage.t.sol#L189-L192
+  function setApprovedRouter(address _router, bool _approved) internal {
+    uint256 writeVal = _approved ? 1 : 0;
+    stdstore.target(address(connext)).sig(connext.approvedRouters.selector).with_key(_router).checked_write(writeVal);
+  }
+
+  function setApprovedAsset(address _asset, bool _approved) internal {
+    uint256 writeVal = _approved ? 1 : 0;
+    stdstore.target(address(connext)).sig(connext.approvedAssets.selector).with_key(_asset).checked_write(writeVal);
+  }
 }
